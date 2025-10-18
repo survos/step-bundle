fe516611829f8286fb85b2862eef574c86008bcc
Tac Tacelosky <tacman@gmail.com>
2025-10-18 09:36:47 -0400

    initial version

---
+++ src/SurvosStepBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Survos\StepBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class SurvosStepBundle extends AbstractBundle
+{
+    protected string $extensionAlias = 'survos_step';
+}
