f3c78e57e41c6913a3fd3b131a2b5b3f110e8749
tacman <info@survos-ci.com>
2025-11-18 09:29:56 +0000

    improve bullets

---
+++ src/Action/Bullet.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Survos\StepBundle\Action;
+
+use Castor\Context;
+use Survos\StepBundle\Metadata\Action;
+use Survos\StepBundle\Util\ArtifactHelper;
+use function Castor\io;
+use function Castor\run;
+use function Castor\task;
+
+
+/** Declarative artifact to snapshot a file after an action runs. */
+final class Bullet extends AbstractAction
+{
+    public function __construct(
+        public string|array $msg = [],
+        public bool $fade=true,
+        public ?string $style='list', // li?
+    ) {
+        $this->msg = is_array($msg) ? $msg : [$msg];
+    }
+
+    public function summary(): string
+    {
+        return json_encode($this);
+        // TODO: Implement summary() method.
+    }
+
+    public function execute(Context $ctx, bool $dryRun = false): void
+    {
+
+        $helper =  ArtifactHelper::fromTaskContext(task(), $ctx);
+        // source is relative to context working directory
+        // asName is relative to artifact root
+        $sourceFilename = $ctx->workingDirectory . '/' . $this->sourcePath;
+        if (file_exists($sourceFilename)) {
+            $content = file_get_contents($sourceFilename);
+            $artifactFilename = $helper->save($this->sourcePath, $content);
+//            dd($artifactFilename, $this->sourcePath, $this->asName);
+            io()->writeln($artifactFilename . " written");
+        } else {
+            $content = run("cat $sourceFilename does not exist");
+        }
+//        $source = $ctx->workingDirectory . '/' . $this->sourcePath;
+    }
+
+    public function viewTemplate(): string
+    {
+        return "bullet.html.twig";
+    }
+}
